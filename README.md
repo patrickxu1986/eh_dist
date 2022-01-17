66076930fe79a571245cffff65872ae29fa543fb
patrickxu1986 <xcpxcp198608@163.com>
2022-01-17 15:17:20 +0800

    202201171517

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 ## eh_highway
 
----
+#### 1. 系统及软件要求
 
-1. Required
+系统: CentOS7.4 ↑
 
-   OS: CentOS7.4 ↑
+软件: docker、nginx、mysql
 
-   SW: docker、nginx、mysql
+#### 2. 配置
 
-2. Config
+执行 config.sh
 
-   ```shell
-   sh config.sh
-   ```
+```shell
+sh config.sh
+```
 
-3. Run
+#### 3.初始化数据库
 
-   ```shell
-   sh run.sh
-   ```
+将init.sql 导入到mysql
 
-   
+进入 /home/java_app/ex_highway文件夹，修改application.yml文件中的数据相关配置信息为你机器上的数据库信息
+
+#### 4. 运行
+
+```shell
+cd /home/java_app/ex_highway
+sh run.sh
+```
+
+
 
--- config.sh
+++ config.sh
@@ -1,21 +1,69 @@
 #!/usr/bin/env bash
-cd
-mkdir java_app
-cd java_app
-mkdir ex_highway
 
-echo "[*] pull custom docker image"
+echo "\n========================================================"
+echo "[*] start pull custom docker image ..."
+echo "========================================================\n"
 docker pull xcpxcp198608/java8
-
-var sql_name = $1
-var sql_pwd = $2
-echo "init mysql database"
-
+echo "\n========================================================"
+echo "[*] docker image pull successfully"
+echo "========================================================\n"
 
 
-echo "create root dir"
+SHPATH=$(cd "$(dirname "$0")"; pwd)
+echo "\n========================================================"
+echo "[*] shell path: $SHPATH"
+echo "========================================================\n"
+
+cd 
+if [ -d "home" ]; then
+	cd home
+fi
+
+if [ ! -d "java_app" ];then
+	mkdir java_app
+fi
+cd java_app
+if [ ! -d "ex_highway" ];then
+	mkdir ex_highway
+fi
+cd ex_highway
+PROJECTPATH=$(cd "$(dirname "$0")"; pwd)
+echo "\n========================================================"
+echo "[*] project path: $PROJECTPATH"
+echo "========================================================\n"
+if [ ! -d "res" ];then
+	mkdir res &&  mkdir res/datapackage res/images res/upload && mkdir res/images/compound
+fi
+echo "\n========================================================"
+echo "[*] dir config successfully"
+echo "========================================================\n"
+
+echo "\n========================================================"
+echo "[*] get remote repo to $SHPATH ... "
+echo "========================================================\n"
+cd $SHPATH
+if [ -d "eh_dist" ]; then
+	rm -rf eh_dist
+fi
+git clone https://github.com/patrickxu1986/eh_dist
 
 
-echo "pull the latest release jar"
+echo "\n========================================================"
+echo "[*] project file copying ..."
+echo "========================================================\n"
+cd $PROJECTPATH
+cp -rf $SHPATH/eh_dist/* .
+echo "\n========================================================"
+echo "[*] project file copy successfully"
+echo "========================================================\n"
 
-cd /home/java_app/ex_highway/
+cd $SHPATH
+if [ -d "eh_dist" ]; then
+	rm -rf eh_dist
+fi
+
+echo "\n========================================================"
+echo "[*] next step is run project, use follow command"
+echo "[*] cd $PROJECTPATH"
+echo "[*] sh run.sh"
+echo "========================================================\n"
